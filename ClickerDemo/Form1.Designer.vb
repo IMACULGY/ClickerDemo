aa5d677bff2fc149f9f93620e6e311079fd74bd8
IMACULGY <drewisaacson@live.com>
2019-03-05 22:19:35 -0500

    Added support for hotkeys.

--- ClickerDemo/Form1.Designer.vb
+++ ClickerDemo/Form1.Designer.vb
@@ -22,92 +22,138 @@ Partial Class Form1
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
-		Me.components = New System.ComponentModel.Container()
-		Me.TextBox1 = New System.Windows.Forms.TextBox()
-		Me.Label1 = New System.Windows.Forms.Label()
-		Me.Label2 = New System.Windows.Forms.Label()
-		Me.TextBox2 = New System.Windows.Forms.TextBox()
-		Me.Button1 = New System.Windows.Forms.Button()
-		Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-		Me.Label3 = New System.Windows.Forms.Label()
-		Me.SuspendLayout()
-		'
-		'TextBox1
-		'
-		Me.TextBox1.Location = New System.Drawing.Point(178, 51)
-		Me.TextBox1.Name = "TextBox1"
-		Me.TextBox1.Size = New System.Drawing.Size(120, 20)
-		Me.TextBox1.TabIndex = 0
-		'
-		'Label1
-		'
-		Me.Label1.Location = New System.Drawing.Point(41, 54)
-		Me.Label1.Name = "Label1"
-		Me.Label1.Size = New System.Drawing.Size(98, 17)
-		Me.Label1.TabIndex = 1
-		Me.Label1.Text = "Number of Clicks"
-		Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
-		'
-		'Label2
-		'
-		Me.Label2.Location = New System.Drawing.Point(41, 112)
-		Me.Label2.Name = "Label2"
-		Me.Label2.Size = New System.Drawing.Size(98, 17)
-		Me.Label2.TabIndex = 2
-		Me.Label2.Text = "Interval (ms)"
-		Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
-		'
-		'TextBox2
-		'
-		Me.TextBox2.Location = New System.Drawing.Point(178, 112)
-		Me.TextBox2.Name = "TextBox2"
-		Me.TextBox2.Size = New System.Drawing.Size(120, 20)
-		Me.TextBox2.TabIndex = 3
-		'
-		'Button1
-		'
-		Me.Button1.ForeColor = System.Drawing.SystemColors.ControlText
-		Me.Button1.Location = New System.Drawing.Point(76, 175)
-		Me.Button1.Name = "Button1"
-		Me.Button1.Size = New System.Drawing.Size(108, 37)
-		Me.Button1.TabIndex = 4
-		Me.Button1.Text = "Click!"
-		Me.Button1.UseVisualStyleBackColor = True
-		'
-		'Timer1
-		'
-		'
-		'Label3
-		'
-		Me.Label3.Location = New System.Drawing.Point(240, 187)
-		Me.Label3.Name = "Label3"
-		Me.Label3.Size = New System.Drawing.Size(58, 18)
-		Me.Label3.TabIndex = 5
-		Me.Label3.Text = "Label3"
-		'
-		'Form1
-		'
-		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(359, 247)
-		Me.Controls.Add(Me.Label3)
-		Me.Controls.Add(Me.Button1)
-		Me.Controls.Add(Me.TextBox2)
-		Me.Controls.Add(Me.Label2)
-		Me.Controls.Add(Me.Label1)
-		Me.Controls.Add(Me.TextBox1)
-		Me.Name = "Form1"
-		Me.Text = "Clicker"
-		Me.ResumeLayout(False)
-		Me.PerformLayout()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.InfoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HotkeysToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(178, 51)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(120, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(41, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Number of Clicks"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(41, 112)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Interval (ms)"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(178, 112)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(120, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Button1.Location = New System.Drawing.Point(76, 175)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(108, 37)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Click!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(240, 187)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 18)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Label3"
+        '
+        'Timer2
+        '
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.InfoToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(359, 24)
+        Me.MenuStrip1.TabIndex = 6
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'InfoToolStripMenuItem
+        '
+        Me.InfoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HotkeysToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.InfoToolStripMenuItem.Name = "InfoToolStripMenuItem"
+        Me.InfoToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.InfoToolStripMenuItem.Text = "File"
+        '
+        'HotkeysToolStripMenuItem
+        '
+        Me.HotkeysToolStripMenuItem.Name = "HotkeysToolStripMenuItem"
+        Me.HotkeysToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.HotkeysToolStripMenuItem.Text = "Hotkeys"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(359, 247)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Clicker"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
-	End Sub
+    End Sub
 
-	Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox1 As TextBox
 	Friend WithEvents Label1 As Label
 	Friend WithEvents Label2 As Label
 	Friend WithEvents TextBox2 As TextBox
 	Friend WithEvents Button1 As Button
 	Friend WithEvents Timer1 As Timer
 	Friend WithEvents Label3 As Label
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents InfoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HotkeysToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
 End Class
--- ClickerDemo/Form1.vb
+++ ClickerDemo/Form1.vb
@@ -4,14 +4,10 @@ Public Class Form1
     <DllImport("user32.dll")>
     Private Shared Sub mouse_event(dwFlags As UInteger, dx As UInteger, dy As UInteger, dwData As UInteger, dwExtraInfo As Integer)
     End Sub
+    <DllImport("user32.dll")>
+    Public Shared Function GetAsyncKeyState(ByVal vKey As System.Windows.Forms.Keys) As Short
+    End Function
 
-
-
-    Private WithEvents MouseDetector As MouseDetector
-    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal vkey As Integer) As Short
-    Public Declare Auto Function SetCursorPos Lib "User32.dll" (ByVal X As Integer, ByVal Y As Integer) As Long
-    Public Declare Auto Function GetCursorPos Lib "User32.dll" (ByRef lpPoint As Point) As Long
-    'Public Declare Sub apimouse_event Lib "user32.dll" Alias "mouse_event" (ByVal dwFlags As Long, ByVal dx As Long, ByVal dy As Long, ByVal cButtons As Long, ByVal dwExtraInfo As Long)
     Public Const MOUSEEVENTF_LEFTDOWN = &H2 ' left button down
     Public Const MOUSEEVENTF_LEFTUP = &H4 ' left button up
 
@@ -26,6 +22,9 @@ Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Label3.Text = i
+        Me.KeyPreview = True
+        Timer2.Interval = 100
+        Timer2.Start()
     End Sub
 
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
@@ -37,34 +36,71 @@ Public Class Form1
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        clicks = Integer.Parse(TextBox1.Text)
-        Dim interval As Integer = Integer.Parse(TextBox2.Text)
-
-        'Makes sure the button is only clicked once.
-        If Not flag Then
-            flag = True
-            Timer1.Interval = interval
-            Timer1.Start()
-        End If
-
+        StartAutoClicker()
     End Sub
 
     Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
 
     End Sub
 
+    'Timer will stop if # of clicks exceeds the entered value OR hotkey is entered.
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        If i < clicks Then
-            i = i + 1
-            Label3.Text = i
-            EmulateClickEvent()
-        Else
+        Dim Hotkey As Boolean
+        Hotkey = GetAsyncKeyState(Keys.F8)
+        If Hotkey = True Then
             Timer1.Stop()
             flag = False
             i = 0
+            Timer2.Start()
+        Else
+            If i < clicks Then
+                i = i + 1
+                Label3.Text = i
+                EmulateClickEvent()
+            Else
+                Timer1.Stop()
+                flag = False
+                i = 0
+                Timer2.Start()
+            End If
         End If
     End Sub
 
+    'Start the autoclicking timer.
+    Private Sub StartAutoClicker()
 
+        If (TextBox1.Text = "" Or TextBox2.Text = "" Or Not (IsNumeric(TextBox2.Text)) Or Not (IsNumeric(TextBox1.Text))) Then
+            MsgBox("Please enter a valid number value for clicks and/or interval", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "ERROR")
+        ElseIf (Integer.Parse(TextBox1.Text) <= 0 Or Integer.Parse(TextBox2.Text) <= 0) Then
+            MsgBox("Please enter a number other than 0 for clicks and/or interval", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "ERROR")
+        Else
+            clicks = Integer.Parse(TextBox1.Text)
+            Dim interval As Integer = Integer.Parse(TextBox2.Text)
 
+            'Makes sure the button/keybind is only clicked once.
+            If Not flag Then
+                flag = True
+                Timer2.Stop()
+                Timer1.Interval = interval
+                Timer1.Start()
+            End If
+        End If
+    End Sub
+
+    'Pressing F7 will start the autoclicker, pressing F8 will stop it.
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Dim Hotkey As Boolean
+        Hotkey = GetAsyncKeyState(Keys.F7)
+        If Hotkey = True Then
+            StartAutoClicker()
+        End If
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub HotkeysToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HotkeysToolStripMenuItem.Click
+        MsgBox("F7 to start the clicker, F8 to stop it. Hotkeys can be pressed when the program is in the background. Make sure you have correct values for # of Clicks and Interval before starting the clicker.", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Hotkeys")
+    End Sub
 End Class
